21cbff1b8c1e900945d0deaf60ba079fa52cea85
Anne Thor <anne.thor@gmail.com>
2018-04-01 15:30:54 -0700

    completed checkpoint-audio-playback, added functionality to start and stop audio when user clicks the number of the song

--- src/components/Album.js
+++ src/components/Album.js
@@ -6,15 +6,48 @@ class Album extends Component {
     super(props);
 
     const album = albumData.find( album => {return album.slug === this.props.match.params.slug });
-    this.state={ album: album };
+    this.state={
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
+    };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
 
   }
+
+  play() {
+    this.audioElement.play();
+    this.setState( {isPlaying: true} );
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState( {isPlaying: false} );
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState( {currentSong: song} );
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      return this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
+  }
+
 
   render() {
     return (
       <div>
       <section id="album-info">
-        <img id="album-cover-art" src={this.state.album.albumCover}/>
+        <img id="album-cover-art" alt="album cover" src={this.state.album.albumCover}/>
         <div className="album-details">
           <h1 id="album-title">{this.state.album.title}</h1>
           <h2 className="artist">{this.state.album.artist}</h2>
@@ -29,9 +62,15 @@ class Album extends Component {
         </colgroup>
         <tbody>
           {
-            this.state.album.songs.map( (song, i) =>
-            {return <tr className="song-details">
-              <td className="song-number">{i+1}.</td>
+            this.state.album.songs.map( (song, index) =>
+            {return <tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
+              <td className="song-actions">
+                <button>
+                  <span className="song-number">{index+1}.</span>
+                  <span className="ion-play"></span>
+                  <span className="ion-pause"></span>
+                </button>
+              </td>
               <td className="song-title">{song.title}</td>
               <td className="song-duration">{Math.floor(song.duration/60)}:{Math.floor(song.duration%60)}</td>
               </tr>} )
